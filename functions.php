37b95a6b66d294c96e4f45d9bc42d5a341765b08
Todd Murphy <tam1998@comcast.net>
2020-02-11 23:17:48 -0500

    testing

--- functions.php
+++ functions.php
@@ -66,7 +66,7 @@ function submitRequest($user, $location, $area, $category, $description, $files)
 
         $rows = $q->rowCount();
         
-        $sql = "INSERT INTO requests(rid, user, location, area, category, description, files) VALUES ($rows + 1, '$user', '$location', '$category', '$description', '')";
+        $sql = "INSERT INTO requests(rid, user, location, area, category, description, files) VALUES ($rows + 1, '$user', '$location', '$area' '$category', '$description', '')";
         $q = $db->prepare($sql);
 
         if($q->execute() === false){
