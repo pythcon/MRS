2880e5ebc3380ecadaa38bc4404e2d2ab1610088
Todd Murphy <tam1998@comcast.net>
2020-02-12 22:16:43 -0500

    changed table

--- allrequests.php
+++ allrequests.php
@@ -72,7 +72,7 @@
                             <div class="mx-auto">
                                 <!--Table Printout-->
                                 <?php
-                                    echo "<table class='table table-bordered'>";
+                                    echo "<table class='table'>";
                                     echo "<tr><td>RID</td><td>Status</td><td>Location</td><td>Description</td><td>Date</td><td>Complete Date</td></tr>";
                                     $requestList = listAllRequests($role);
                                     echo $requestList;
